4495698eb2e6a81c411309121f366d3579906171
Mauricio Barrera <mauricio.barrerag@gmail.com>
2016-08-20 00:13:43 -0500

    Lesson #58 - Mobydock

---
+++ MobyDock/mobydock/config/settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'pass$word01'
+
+APP_NAME = 'My First Docker App'
+
+DEBUG = True
+TESTING = False
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://mobydock:pass123@postgres:5432/mobydock'
+REDIS_URL = 'redis://redis:6379/0'
